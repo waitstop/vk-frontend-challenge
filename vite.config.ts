3d104fa0c3244ac82631255facc9423e0463736d
Aleksandr Kazakov <emtyxd@mail.ru>
2025-01-16 02:26:50 +0300

    build: added script for deploy, changed base in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [TanStackRouterVite(), react()],
+  base: "/vk-frontend-challenge",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
